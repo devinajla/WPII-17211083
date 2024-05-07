a2638363f650af56f196a9693caeba47b45c99c2
devinajla <163952871+devinajla@users.noreply.github.com>
2024-05-07 08:53:14 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Devina Najla Nabila
+              Saya tinggal di Pamulang
+              olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
